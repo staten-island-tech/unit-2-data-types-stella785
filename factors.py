18d4061a162255fd531241a68bc96ef17e3178ff
stella785 <stellac35@nycstudents.net>
2024-09-25 09:20:52 -0400

    finished factors question

--- factors.py
+++ factors.py
@@ -1,7 +1,14 @@
-number = int(input("Enter a number"))
+def find_factors(n):
+    if n <= 0:
+        return "Enter a positive whole number"
 
-factors = []
-for i in range(1, number + 1):
-    if number % i == 0:
-        factors.append(i)
+    factors = []
+    for i in range(1, n + 1):
+        if n % i == 0:
+            factors.append(i)
+    return factors
 
+number = int(input("Enter a number: "))
+result = find_factors(number)
+
+print(result)
